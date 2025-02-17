f95cfc5e98456c0b3284239dd83161bb3321a19c
Matteo Vignaga <114475610+MatteoVignaga@users.noreply.github.com>
2025-02-17 21:29:20 +0100

    Full min conflicts algorithm with pygame GUI and visualization

--- Main.py
+++ Main.py
@@ -1,28 +1,35 @@
 import random
 import time
 from math import floor
+
+import pygame
 from pygame import Vector2
+from win32api import mouse_event
 
 #board will be nxn, queens will be an n sized list
 n = 10
+square_size = 600/n
+max_steps = 1000
+colors = {
+    "GRAY": (200, 200, 200),
+    "GREEN": (0, 100, 0),
+    "BLACK": (70, 70, 70)
+}
 
-def main():
-    #empty board
-    board = [[0 for _ in range(n)] for _ in range(n)]
-    show_board(board)
+def generate_queens():
+    return [Vector2(i, random.randint(0, n - 1)) for i in range(n)]
 
-    #generate queens in random spots: place one queen per column at a random height
-    queens = [Vector2(i, random.randint(0,n-1)) for i in range(n)]
-    print(queens)
 
-    #place the queens on the board
+def place_queens(queens, board):
     for queen in queens:
         board[floor(queen.y)][floor(queen.x)] = 1
-        show_board(board)
 
-    #start algorithm
-    res = min_conflicts(500, board, queens)
-    if res is None: print("Non è stato possibile risolvere il CSP con questi steps")
+
+def reset():
+    board = [[0 for _ in range(n)] for _ in range(n)]
+    queens = generate_queens()
+    place_queens(queens, board)
+    return board, queens
 
 
 def count_same_row_queens(queen, board):
@@ -83,9 +90,18 @@ def choose_new_position(queen, board):
 
 
 def show_board(board):
-    for row in board:
-        print(row)
-    print()
+    board_size = (600, 600)
+    grid = pygame.Rect(0, 0, board_size[0], board_size[1])
+    grid.center = (int(display.get_width() / 2), int(display.get_height() / 2))
+    pygame.draw.rect(display, colors["BLACK"], grid, 3)
+
+    for i in range(len(board)):
+        for ii in range (len(board[i])):
+            square = pygame.Rect(0, 0, square_size, square_size)
+            square.topleft = (int(grid.topleft[0] + square_size*ii), int(grid.topleft[1] + square_size*i))
+            if board[i][ii] == 1: pygame.draw.rect(display, colors["GREEN"], square)
+            pygame.draw.rect(display, colors["BLACK"], square, 1)
+
 
 
 def min_conflicts(max_steps, board, queens):
@@ -95,7 +111,7 @@ def min_conflicts(max_steps, board, queens):
         if len(conflicted) == 0: return board
 
         #select a queen whose position causes a conflict
-        queen = conflicted[random.randint(0, len(conflicted)-1)]
+        queen = random.choice(conflicted)
 
         #select the position for this queen that minimizes conflicts
         new_pos = choose_new_position(queen, board)
@@ -108,8 +124,60 @@ def min_conflicts(max_steps, board, queens):
         board[int(new_pos.y)][int(new_pos.x)] = 1
 
         #show board at each step
+        display.fill(colors["GRAY"])
         show_board(board)
-        #time.sleep(1)
+        pygame.display.flip()
+        time.sleep(0.1)
     return None
 
+
+def draw_start_button(button_rect):
+    pygame.draw.rect(display, colors["BLACK"], button_rect, 3, 3)
+    font = pygame.font.Font(None, 30)
+    text = font.render("start", True, colors["BLACK"])
+    display.blit(text, (button_rect.x + 25, button_rect.y + 10))
+
+
+def draw_reset_button(button_rect):
+    pygame.draw.rect(display, colors["BLACK"], button_rect, 3, 3)
+    font = pygame.font.Font(None, 30)
+    text = font.render("reset", True, colors["BLACK"])
+    display.blit(text, (button_rect.x + 25, button_rect.y + 10))
+
+
+def main():
+    display.fill(colors["GRAY"])
+    running = True
+    clock = pygame.time.Clock()
+
+    # empty board
+    board = [[0 for _ in range(n)] for _ in range(n)]
+    # generate queens in random spots: place one queen per column at a random height
+    queens = generate_queens()
+    # place the queens on the board
+    place_queens(queens, board)
+
+    #start button
+    start_button_rect = pygame.Rect(20, 20, 100, 40)
+    reset_button_rect = pygame.Rect(20, 80, 100, 40)
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if start_button_rect.collidepoint(event.pos):
+                    res = min_conflicts(max_steps, board, queens)
+                    if res is None: print("The algorithm stopped without being able to solve the CSP.")
+                if reset_button_rect.collidepoint(event.pos): board, queens = reset()
+
+        display.fill(colors["GRAY"])
+        show_board(board)
+        draw_start_button(start_button_rect)
+        draw_reset_button(reset_button_rect)
+        pygame.display.flip()
+        clock.tick(1)
+
+pygame.init()
+display = pygame.display.set_mode((900, 700))
 main()
