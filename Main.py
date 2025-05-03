ada728b36e8deb0b94a990466b34fc753fcef7ac
Matteo Vignaga <114475610+MatteoVignaga@users.noreply.github.com>
2025-05-04 01:10:20 +0200

    finished backtracking search, had to move some methods into a common file called utils to avoid circular import

--- Main.py
+++ Main.py
@@ -1,32 +1,21 @@
 import random
 import string
 import time
-from math import floor
 
 import pygame
 from pygame import Vector2
-from win32api import mouse_event
+
+from BacktrackingSearch import BacktrackingSearch
+from utils import *
 
 #board will be nxn, queens will be an n sized list
 #queens are represented as a couple (x,y) representing their coordinates
-n = 20
-square_size = 600/n
+
 max_steps = 1000
-colors = {
-    "GRAY": (200, 200, 200),
-    "GREEN": (0, 100, 0),
-    "BLACK": (70, 70, 70),
-    "LOCK GRAY": (180, 180, 180)
-}
 
 #queens are represented by x-column and y-row coordinates
 def generate_queens():
     return [Vector2(i, random.randint(0, n - 1)) for i in range(n)]
-
-#board is printed row by row!
-def place_queens(queens, board):
-    for queen in queens:
-        board[floor(queen.y)][floor(queen.x)] = 1
 
 
 def reset():
@@ -79,56 +68,6 @@ def count_conflicts(queen, board):
     return same_row + same_column + same_diagonal + same_antidiagonal
 
 
-def lock_same_row(constraints_board, queen):
-    row_index = int(queen.y)
-    for col_index in range(len(constraints_board[row_index])):
-        constraints_board[row_index][col_index] -= 1
-
-
-def lock_same_column(constraints_board, queen):
-    col_index = int(queen.x)
-    for row_index in range(len(constraints_board)):
-        constraints_board[row_index][col_index] -= 1
-
-
-def lock_same_diagonal_and_antidiagonal(constraints_board, queen):
-    for r in range(len(constraints_board)):
-        for c in range(len(constraints_board)):
-            if (r - c == queen.y - queen.x) or (r + c == queen.y + queen.x):
-                constraints_board[r][c] -= 1
-
-
-def lock_cells(constraints_board, queen):
-    lock_same_row(constraints_board, queen)
-    lock_same_column(constraints_board, queen)
-    lock_same_diagonal_and_antidiagonal(constraints_board, queen)
-
-
-def unlock_same_row(constraints_board, queen):
-    row_index = int(queen.y)
-    for col_index in range(len(constraints_board[row_index])):
-        constraints_board[row_index][col_index] += 1
-
-
-def unlock_same_column(constraints_board, queen):
-    col_index = int(queen.x)
-    for row_index in range(len(constraints_board)):
-        constraints_board[row_index][col_index] += 1
-
-
-def unlock_same_diagonal_and_antidiagonal(constraints_board, queen):
-    for r in range(len(constraints_board)):
-        for c in range(len(constraints_board)):
-            if (r - c == queen.y - queen.x) or (r + c == queen.y + queen.x):
-                constraints_board[r][c] += 1
-
-
-def unlock_cells(constraints_board, queen):
-    unlock_same_row(constraints_board, queen)
-    unlock_same_column(constraints_board, queen)
-    unlock_same_diagonal_and_antidiagonal(constraints_board, queen)
-
-
 def update_conflicted(queens, board):
     return [queen for queen in queens if count_conflicts(queen, board) > 0]
 
@@ -144,25 +83,6 @@ def choose_new_position(queen, board):
         if min is None or conflicts[pos] < conflicts[min]:
             min = pos
     return Vector2(min[0], min[1])
-
-
-def draw_board(board, constraints_board):
-    board_size = (600, 600)
-    grid = pygame.Rect(0, 0, board_size[0], board_size[1])
-    grid.center = (int(display.get_width() / 2) + 100, int(display.get_height() / 2))
-
-    for i in range(len(board)):
-        for ii in range (len(board[i])):
-            square = pygame.Rect(0, 0, square_size, square_size)
-            square.topleft = (int(grid.topleft[0] + square_size*ii), int(grid.topleft[1] + square_size*i))
-            if board[i][ii] == 1:
-                pygame.draw.rect(display, colors["GREEN"], square)
-            elif constraints_board[i][ii] < 0:
-                pygame.draw.rect(display, colors["LOCK GRAY"], square)
-            pygame.draw.rect(display, colors["BLACK"], square, 1)
-
-    pygame.draw.rect(display, colors["BLACK"], grid, 3)
-
 
 
 def min_conflicts(max_steps, board, constraints_board):
@@ -197,7 +117,7 @@ def min_conflicts(max_steps, board, constraints_board):
 
         #show board at each step
         display.fill(colors["GRAY"])
-        draw_board(board, constraints_board)
+        draw_board(board, constraints_board, display)
         pygame.display.flip()
         time.sleep(0.1)
     return None
@@ -215,10 +135,6 @@ def draw_reset_button(button_rect):
     font = pygame.font.Font(None, 25)
     text = font.render("reset", True, colors["BLACK"])
     display.blit(text, (button_rect.x + 25, button_rect.y + 10))
-
-
-def backtracking_search():
-    print("You wanna go with backtracking search?")
 
 
 def main():
@@ -244,11 +160,13 @@ def main():
                     res = min_conflicts(max_steps, board, constraints_board)
                     if res is None: print("The algorithm stopped without being able to solve the CSP.")
                 if start_backtracking_search_button_rect.collidepoint(event.pos):
-                    res = backtracking_search()
+                    algorithm = BacktrackingSearch(board, constraints_board, n, display)
+                    res = algorithm.solve()
+                    if not res: print("The algorithm stopped without being able to solve the CSP.")
                 if reset_button_rect.collidepoint(event.pos): board, constraints_board, queens = reset()
 
         display.fill(colors["GRAY"])
-        draw_board(board, constraints_board)
+        draw_board(board, constraints_board, display)
         draw_start_button(start_minconflicts_button_rect, "minconflicts")
         draw_start_button(start_backtracking_search_button_rect, "backtracking search")
         #draw_reset_button(reset_button_rect)
